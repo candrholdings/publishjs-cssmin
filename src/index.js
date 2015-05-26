d4d329b12af3ee04880ae01daffe55b99b876c78
compulim <compulim@hotmail.com>
2015-05-26 17:10:39 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+!function (cssmin, number, time) {
+    'use strict';
+
+    module.exports = function (inputs, outputs, callback) {
+        var that = this;
+
+        inputs = inputs.newOrChanged;
+
+        Object.getOwnPropertyNames(inputs).forEach(function (filename) {
+            var startTime = Date.now(),
+                original = inputs[filename].buffer,
+                minified = outputs[filename] = new Buffer(cssmin(inputs[filename].buffer.toString()));
+
+            that.log([
+                'Minified ',
+                filename,
+                ', took ',
+                time.humanize(Date.now() - startTime),
+                ' (',
+                number.bytes(original.length),
+                ' -> ',
+                number.bytes(minified.length),
+                ', ',
+                (((minified.length / original.length) - 1) * 100).toFixed(1),
+                '%)'
+            ].join(''));
+        });
+
+        callback(null, outputs);
+    };
+}(
+    require('cssmin'),
+    require('./util/number'),
+    require('./util/time')
+);
